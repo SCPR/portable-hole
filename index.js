0be72bd3bdf6309e01c8765938e0bb2632b9ebf6
Ben Titcomb <btitcomb@scpr.org>
2016-12-10 12:44:11 -0800

    Fix index.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-let cheerio = require('cheerio');
-let $       = cheerio.load(`<a class="embed-placeholder" data-placement="replace" data-service="youtube" href="https://www.youtube.com/watch?v=e4lqyib3QPE" title="https://www.youtube.com/watch?v=e4lqyib3QPE">RAMMSTEIN IST FUR IMMER</a>`);
-$.ajax      = require('najax');
-
-module.exports = require('./dist/portable-holes');
+module.exports = require('./lib/portable-holes');
